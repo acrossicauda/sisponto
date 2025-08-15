8959dd2b5771f66d983fa4b6aeaa1a2646c02f0c
Tiago Sousa <tiago.sousa@ext-onfly.com>
2025-08-15 18:23:36 -0300

    Teste pipeline

--- database/migrations/2025_01_17_174307_create_categories_table.php
+++ database/migrations/2025_01_17_174307_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->default('');
             $table->string('description')->nullable();
             $table->integer('status')->default(1);
             $table->timestamps();
